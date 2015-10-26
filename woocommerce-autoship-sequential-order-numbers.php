cf2933dcdb02ba4235c6700e2f3a098f2ad91366
Jonathan Baltazar <jonrules@gmail.com>
2015-10-26 11:19:36 -0500

    remove plugin dependencies, check if wc_seq_order_number_pro function exists

--- woocommerce-autoship-sequential-order-numbers.php
+++ woocommerce-autoship-sequential-order-numbers.php
@@ -8,38 +8,35 @@ Author: Patterns in the Cloud
 Author URI: http://patternsinthecloud.com
 License: Single-site
 */
+	
+/**
+ * Activate hook
+ */
+function wc_autoship_sequential_order_numbers_activate() {
+	
+}
+register_activation_hook( __FILE__, 'wc_autoship_sequential_order_numbers_activate' );
 
-include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-if ( is_plugin_active( 'woocommerce-autoship/woocommerce-autoship.php' )
-		&& is_plugin_active( 'woocommerce-sequential-order-numbers-pro/woocommerce-sequential-order-numbers.php' ) ) {
+/**
+ * Deactivate hook
+ */
+function wc_autoship_sequential_order_numbers_deactivate() {
 	
-	/**
-	 * Activate hook
-	 */
-	function wc_autoship_sequential_order_numbers_activate() {
-		
-	}
-	register_activation_hook( __FILE__, 'wc_autoship_sequential_order_numbers_activate' );
+}
+register_deactivation_hook( __FILE__, 'wc_autoship_sequential_order_numbers_deactivate' );
+
+/**
+ * Uninstall hook
+ */
+function wc_autoship_sequential_order_numbers_uninstall() {
 	
-	/**
-	 * Deactivate hook
-	 */
-	function wc_autoship_sequential_order_numbers_deactivate() {
-		
-	}
-	register_deactivation_hook( __FILE__, 'wc_autoship_sequential_order_numbers_deactivate' );
-	
-	/**
-	 * Uninstall hook
-	 */
-	function wc_autoship_sequential_order_numbers_uninstall() {
-		
-	}
-	register_uninstall_hook( __FILE__, 'wc_autoship_sequential_order_numbers_uninstall' );
-	
-	function wc_autoship_sequential_order_numbers_set_order_number( $order_id  ) {
+}
+register_uninstall_hook( __FILE__, 'wc_autoship_sequential_order_numbers_uninstall' );
+
+function wc_autoship_sequential_order_numbers_set_order_number( $order_id  ) {
+	if ( function_exists( 'wc_seq_order_number_pro' ) ) {
 		$wc_seq_order_number_pro = wc_seq_order_number_pro();
 		$wc_seq_order_number_pro->set_sequential_order_number( $order_id ); 
 	}
-	add_action( 'wc_autoship_new_order', 'wc_autoship_sequential_order_numbers_set_order_number', 10, 1 );
 }
+add_action( 'wc_autoship_new_order', 'wc_autoship_sequential_order_numbers_set_order_number', 10, 1 );
